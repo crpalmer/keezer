549242c77b0cb131f67ddd67ec6e8b2eb337c8a4
Christopher R. Palmer <crpalmer@gmail.com>
2015-07-05 11:33:41 -0400

    Add editing and listing of beers

--- ui/app/controllers/beers_controller.rb
+++ ui/app/controllers/beers_controller.rb
@@ -2,10 +2,27 @@ class BeersController < ApplicationController
   def new
   end
 
+  def index
+    @beers = Beer.order("name").all
+  end
+
   def create
     @beer = Beer.new(beer_params)
     @beer.save
     redirect_to @beer
+  end
+
+  def edit
+    @beer = Beer.find(params[:id])
+  end
+
+  def update
+    @beer = Beer.find(params[:id])
+    if @beer.update(beer_params)
+      redirect_to @beer
+    else
+      render 'edit'
+    end
   end
 
   def show
