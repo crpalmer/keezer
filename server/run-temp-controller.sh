69ae98819e5ac0d0e5bd2fe334d616c6f545fdf6
Christopher R. Palmer <crpalmer@gmail.com>
2017-06-06 06:22:24 -0400

    server: Add temp-controller configs & startup script

---
+++ server/run-temp-controller.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Add this to /etc/rc.local:
+#
+# sudo -u crpalmer /opt/keezer/run-temp-controller.sh
+
+(
+while true
+do
+    cd /opt/keezer
+    ./temp-controller temp-controller-configs/`cat temp-controller.cfg`.cfg
+    sleep 2
+done
+) > /tmp/temp-controller.log 2>&1
