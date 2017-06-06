b8221f532e79e46423065bdc2c293d10907182ce
Christopher R. Palmer <crpalmer@gmail.com>
2017-06-06 06:19:44 -0400

    server: chromium changed to chromium-browser

--- server/lxde-autostart.sh
+++ server/lxde-autostart.sh
@@ -5,5 +5,5 @@ xset s off
 xset -dpms
 xset s noblank
 mkdir /tmp/keeezer-temporary-dir
-chromium --user-data-dir=/tmp/keezer-temporary-dir --kiosk http://127.0.0.1:3000
+chromium-browser --user-data-dir=/tmp/keezer-temporary-dir --kiosk http://127.0.0.1:3000
 ) > /tmp/chromium.log 2>&1
