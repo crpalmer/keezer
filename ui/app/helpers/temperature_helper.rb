cfd0f672e87069f49e6ca0e12ac2310a0d8f2c5b
Christopher R. Palmer <crpalmer@gmail.com>
2020-04-26 08:23:27 -0400

    ui: make the temperatures use the slow but accurate readings

--- ui/app/helpers/temperature_helper.rb
+++ ui/app/helpers/temperature_helper.rb
@@ -1,24 +1,24 @@
 module TemperatureHelper
   def keezer_temperature
-    number_with_precision(File.read('/mnt/1wire/28.FF3DCC6B1403/fasttemp'), precision: 1)
+    number_with_precision(File.read('/mnt/1wire/28.FF3DCC6B1403/temperature'), precision: 1)
     rescue
        '???'
   end
 
   def tower_temperature
-    number_with_precision(File.read('/mnt/1wire/28.FFAA976B1403/fasttemp'), precision: 1)
+    number_with_precision(File.read('/mnt/1wire/28.FFAA976B1403/temperature'), precision: 1)
     rescue
        '???'
   end
 
   def chamber_temperature
-    number_with_precision(File.read('/mnt/1wire/28.FFCEBF6B1403/fasttemp'), precision: 1)
+    number_with_precision(File.read('/mnt/1wire/28.FFCEBF6B1403/temperature'), precision: 1)
     rescue
        '???'
   end
 
   def wort_temperature
-    number_with_precision(File.read('/mnt/1wire/28.FFB6AD6B1403/fasttemp'), precision: 1)
+    number_with_precision(File.read('/mnt/1wire/28.FFB6AD6B1403/temperature'), precision: 1)
     rescue
        '???'
   end
