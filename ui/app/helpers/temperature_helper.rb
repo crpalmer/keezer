e551ad646cc223ea593d5edb4e4f71b94d4f1293
Christopher R. Palmer <crpalmer@gmail.com>
2020-04-26 08:19:10 -0400

    ui: add temperature readings for the fermentation chamber

--- ui/app/helpers/temperature_helper.rb
+++ ui/app/helpers/temperature_helper.rb
@@ -12,10 +12,14 @@ module TemperatureHelper
   end
 
   def chamber_temperature
-    '???'
+    number_with_precision(File.read('/mnt/1wire/28.FFCEBF6B1403/fasttemp'), precision: 1)
+    rescue
+       '???'
   end
 
   def wort_temperature
-    '???'
+    number_with_precision(File.read('/mnt/1wire/28.FFB6AD6B1403/fasttemp'), precision: 1)
+    rescue
+       '???'
   end
 end
